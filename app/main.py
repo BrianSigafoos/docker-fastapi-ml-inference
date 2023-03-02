c57fe602fd58347838fb2ad59df13f7c6d45d246
Brian Sigafoos <sigafoos@gmail.com>
2023-03-02 14:09:30 -0500

    Add predict endpoint

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ import os
 from datetime import datetime, timezone
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
 from app.env import load_env_vars
+from app.titanic_survival.model import TITANIC_MODEL_VERSION
+from app.titanic_survival.model import predict as titanic_survival_predict
 
 app = FastAPI()
 
@@ -24,6 +27,56 @@ def redirect_to_docs():
     return RedirectResponse(url="/docs")
 
 
+class PassengerIn(BaseModel):
+    """Passenger to predict survival for."""
+
+    PassengerId: int
+    Pclass: int
+    Name: str
+    Sex: str
+    Age: float
+    SibSp: int
+    Parch: int
+    Ticket: str
+    Fare: float
+    Cabin: str
+    Embarked: str
+
+
+class PassengerSurvivalPrediction(BaseModel):
+    """Survival prediction for a single passenger."""
+
+    passenger_id: int
+    survived: bool
+    score: float
+
+
+class SurvivalPredictionByPassengerIds(BaseModel):
+    """Survival prediction for a list of passengers."""
+
+    predictions: list[PassengerSurvivalPrediction]
+    metadata: dict
+
+
+@app.post(
+    "/predict_survival_by_passengers", response_model=SurvivalPredictionByPassengerIds
+)
+def predict_survival_by_passengers(passengers: list[PassengerIn]):
+    # Convert passengers to a list of dicts; this is what the model expects
+    # There must be a better way to do this.
+    passengers = [passenger.dict() for passenger in passengers]
+
+    # Make predictions
+    results = [] if not passengers else titanic_survival_predict(passengers)
+
+    return {
+        "predictions": results,
+        "metadata": {
+            "titanic_model_version": TITANIC_MODEL_VERSION,
+        },
+    }
+
+
 @app.get("/health_check")
 def health_check():
     return {
@@ -32,6 +85,7 @@ def health_check():
         "health": "OK",
         "k8s_env": os.environ.get("K8S_ENV"),
         "python_env": os.environ["PYTHON_ENV"],
+        "titanic_model_version": TITANIC_MODEL_VERSION,
         "version": os.environ.get(
             "APP_REVISION", "Missing $APP_REVISION env var, not set"
         ),
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,61 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.titanic_survival.model import TITANIC_MODEL_VERSION
 
 client = TestClient(app)
+
+payload = [
+    {
+        "PassengerId": 1248,
+        "Pclass": 1,
+        "Name": "Brown, Mrs. John Murray (Caroline Lane Lamson)",
+        "Sex": "female",
+        "Age": 59.0,
+        "SibSp": 2,
+        "Parch": 0,
+        "Ticket": "11769",
+        "Fare": 51.4792,
+        "Cabin": "C101",
+        "Embarked": "S",
+    },
+    {
+        "PassengerId": 1128,
+        "Pclass": 1,
+        "Name": "Warren, Mr. Frank Manley",
+        "Sex": "male",
+        "Age": 64.0,
+        "SibSp": 1,
+        "Parch": 0,
+        "Ticket": "110813",
+        "Fare": 75.25,
+        "Cabin": "D37",
+        "Embarked": "C",
+    },
+]
+
+
+def test_predict_survival_by_passengers():
+    passengers = payload.copy()
+    p1 = passengers[0]
+    p2 = passengers[1]
+
+    response = client.post("/predict_survival_by_passengers", json=payload)
+    # Check that the response status code is 200 OK
+    assert response.status_code == 200
+
+    response = response.json()
+    predictions = response["predictions"]
+    assert predictions[0]["passenger_id"] == p1["PassengerId"]
+    assert predictions[0]["survived"] == True
+    assert predictions[0]["score"] >= 0.90
+
+    assert predictions[1]["passenger_id"] == p2["PassengerId"]
+    assert predictions[1]["survived"] == True
+    assert predictions[1]["score"] <= 0.10
+
+    metadata = response["metadata"]
+    assert metadata["titanic_model_version"] == TITANIC_MODEL_VERSION
 
 
 def test_health_check():
@@ -15,6 +68,7 @@ def test_health_check():
         "health": "OK",
         "k8s_env": None,
         "python_env": "test",
+        "titanic_model_version": TITANIC_MODEL_VERSION,
         "version": "Missing $APP_REVISION env var, not set",
     }
 
