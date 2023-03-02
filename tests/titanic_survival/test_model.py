0b1676682b55ff46f9bcf743d0ded84838ade604
Brian Sigafoos <sigafoos@gmail.com>
2023-03-02 13:17:54 -0500

    Add predict funtion to model in app (#4)

---
+++ tests/titanic_survival/test_model.py
@@ -0,0 +1,46 @@
+# Description: Test the model.py file
+from app.titanic_survival.model import predict
+
+payload = [
+    {
+        "PassengerId": 1248,
+        "Pclass": 1,
+        "Name": "Brown, Mrs. John Murray (Caroline Lane Lamson)",
+        "Sex": "female",
+        "Age": 59.0,
+        "SibSp": 2,
+        "Parch": 0,
+        "Ticket": "11769",
+        "Fare": 51.4792,
+        "Cabin": "C101",
+        "Embarked": "S",
+    },
+    {
+        "PassengerId": 1128,
+        "Pclass": 1,
+        "Name": "Warren, Mr. Frank Manley",
+        "Sex": "male",
+        "Age": 64.0,
+        "SibSp": 1,
+        "Parch": 0,
+        "Ticket": "110813",
+        "Fare": 75.25,
+        "Cabin": "D37",
+        "Embarked": "C",
+    },
+]
+
+
+def test_predict():
+    passengers = payload.copy()
+    p1 = passengers[0]
+    p2 = passengers[1]
+    predictions = predict(passengers)
+
+    assert predictions[0]["passenger_id"] == p1["PassengerId"]
+    assert predictions[0]["survived"] == True
+    assert predictions[0]["score"] >= 0.90
+
+    assert predictions[1]["passenger_id"] == p2["PassengerId"]
+    assert predictions[1]["survived"] == True
+    assert predictions[1]["score"] <= 0.10
